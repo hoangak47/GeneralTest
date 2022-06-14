fed31bf8718735d6cf5c91d32dc68fcaa8f281ed
hoangak47 <57388240+hoangak47@users.noreply.github.com>
2022-06-14 18:03:11 +0700

    test

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,21 @@
 import logo from './logo.svg';
+import { useEffect, useState} from 'react'
 import './App.css';
 
 function App() {
+
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:5000/')
+      .then(res => res.json())
+      .then(data => setData(data))
+  },[])
+  
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>{data}</h1>
     </div>
   );
 }
